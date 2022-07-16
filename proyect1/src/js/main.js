46d8c99796eae3b322d3c806539b0027cf5ba470
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-07-16 12:55:44 +0200

    upload css / js/ html

---
+++ proyect1/src/js/main.js
@@ -0,0 +1,9 @@
+const $menu = document.querySelector("#menu");
+const menuOpen = document.querySelector("#menuOpen");
+
+const menuFunction = function () {
+    $menu.addEventListener("click" , ()=> {
+        $menuOpen.classList.add("active");
+    })
+}
+menuFunction();
