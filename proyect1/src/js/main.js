ecb1c70dafcf9cf2a067b8fc809385946a7a288f
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-07-16 16:33:15 +0200

    Revert "delete"
    This reverts commit cebcb819e30b139dd43da34b822ae80e61810764.

---
+++ proyect1/src/js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener ('load', function () {  
+
+    const $menu = document.querySelector("#menu");
+    const $menuOpen = document.querySelector("#menuOpen");
+    const $menuClose = document.querySelector("#menuOpen__closeLink");
+
+    const menuFunction = function () {
+        $menu.addEventListener("click" , ()=> {
+            $menuOpen.classList.add("active");
+        })
+    }
+    menuFunction();
+
+    const menuFunctionClose = function () {
+        $menuClose.addEventListener("click" , () => {
+            $menuOpen.classList.remove("active");
+        })
+    }
+    menuFunctionClose();
+
+})
